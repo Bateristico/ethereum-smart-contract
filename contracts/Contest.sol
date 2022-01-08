e284b74d1d683299adc0be5d966ae2c9b4edb920
Bateristico <camilo.saa@gmail.com>
2022-01-08 19:30:58 +0100

    first commit

---
+++ contracts/Contest.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.5.16;
+
+// create the contract
+contract Contest {
+    // declaring variable
+    string public contestant;
+
+    // declaring constructor
+    constructor() {
+        contestant = "Mirka";
+    }
+
+
+}
